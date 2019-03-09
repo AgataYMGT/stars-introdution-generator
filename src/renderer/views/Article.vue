ec8cb4bef7cac420612637fb6c7e7cca82a8a935
agata <ymgtech@millenary.net>
2019-03-09 14:02:23 +0900

    [add] ルートとコンポーネントを作成

---
+++ src/renderer/views/Article.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-main">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/scss/commons";
+
+.bg-main {
+  background-color: $gray-100;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
