fc6d085cdce316d1b47f2f93beeac3b3098b17c9
agata <ymgtech@millenary.net>
2019-03-09 12:52:46 +0900

    [add] 質問事項の最終ページに記事生成ボタンを作成

--- src/renderer/components/Create/FormParts.vue
+++ src/renderer/components/Create/FormParts.vue
@@ -3,17 +3,25 @@
     <slot></slot>
     <div class="adjacent-buttons">
       <button type="button" v-if="!isFirstQuestion" @click="$emit('input', value - 1)" class="btn btn-danger rounded-0">戻る</button>
-      <button type="button" @click="$emit('input', value + 1)" class="btn btn-danger rounded-0">次へ</button>
+      <button type="button" v-if="!isLastQuestion" @click="$emit('input', value + 1)" class="btn btn-danger rounded-0">次へ</button>
+      <button type="button" v-if="isLastQuestion" class="btn btn-success rounded-0">記事を生成</button>
+      <input type="text" name="dummy" class="d-none">
     </div>
   </section>
 </template>
 
 <script>
+const lastQuestionId = 4;
+
 export default {
   computed: {
     isFirstQuestion: function() {
       let self = this;
       return self.value - 1 === 0;
+    },
+    isLastQuestion: function() {
+      let self = this;
+      return self.value === 4;
     }
   },
   props: {
